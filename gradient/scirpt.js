caf7575d3a9f8384d85e9006d8f11705ff80248d
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-18 16:24:36 +0500

    create the array

--- gradient/scirpt.js
+++ gradient/scirpt.js
@@ -1,3 +1,10 @@
-function gradient(colors) {
+function gradient(count) {
+    if (count < 2 || count > 5) {
+        alert("The numbers of colors should be between 2 and 5");
+    }
+
+    var colors = [];
+    var steps = 360 / count;
+
     
 }
