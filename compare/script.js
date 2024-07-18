278a2e849c6707002455bbc1d6853bab7e2df86b
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-18 15:12:03 +0500

    return

--- compare/script.js
+++ compare/script.js
@@ -1,11 +1,14 @@
 function numberGenerator(params) {
-    arr = phoneNumbers = [];
+    var phoneNumbers = [];
     while (phoneNumbers.length < count) {
         var phoneNum = '+998' +
             (Math.floor(Math.random() * 90 + 10)).toString().padStart(2, '0') + ' ' +
             (Math.floor(Math.random() * 900 + 100)).toString().padStart(3, '0') + ' ' +
             (Math.floor(Math.random() * 90 + 10)).toString().padStart(2, '0') + ' ' +
             (Math.floor(Math.random() * 90 + 10)).toString().padStart(2, '0');
-        
+        if (phoneNumbers.indexOf(phoneNum) === -1) {
+            phoneNumbers.push(phoneNum);
+        }
     }
+    return phoneNumbers; 
 }
