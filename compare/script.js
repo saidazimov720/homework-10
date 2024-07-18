cffbb91dc8d7bda49d5fcdbe9b338a99e1404cbd
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-18 14:11:42 +0500

    add the test logic

--- compare/script.js
+++ compare/script.js
@@ -1,4 +1,11 @@
 function numberGenerator(params) {
     arr = phoneNumbers = [];
-    
+    while (phoneNumbers.length < count) {
+        var phoneNum = '+998' +
+            (Math.floor(Math.random() * 90 + 10)).toString().padStart(2, '0') + ' ' +
+            (Math.floor(Math.random() * 900 + 100)).toString().padStart(3, '0') + ' ' +
+            (Math.floor(Math.random() * 90 + 10)).toString().padStart(2, '0') + ' ' +
+            (Math.floor(Math.random() * 90 + 10)).toString().padStart(2, '0');
+        
+    }
 }
