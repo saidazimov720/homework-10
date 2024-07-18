77f3df1c0668c8c4f7efb128f6ecb101c293cac9
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-18 20:20:30 +0500

    function hsl to rgb

--- gradient/scirpt.js
+++ gradient/scirpt.js
@@ -7,10 +7,15 @@ function gradient(count) {
     var colors = [];
     var huesteps = 360 / count;
 
-    var hsltoRgb = function(hsl){
+    var hsltoRgb = function (hsl) {
         var h = hsl.match(/\d+/g).map(Number)[0];
-        var h = hsl.match(/\d+/g).map(Number)[1];
-        var h = hsl.match(/\d+/g).map(Number)[2];
+        var s = hsl.match(/\d+/g).map(Number)[1];
+        var l = hsl.match(/\d+/g).map(Number)[2];
+        var a = s * Math.min(l / 100, 1 - l / 100);
+        var f = function (n) {
+            return l - a * Math.max(-1, Math.min(Math.cos((h / 30 + n) * Math.PI / 6), 1, 1));
+        };
+        return [f(0) * 255, f(8) * 255, f(4) * 255];
     }
 }
 
@@ -23,7 +28,7 @@ function generateGradient(colors) {
         var startColor = colors[colorIndex];
         var endColor = colors[colorIndex + 1];
 
-        var weight = (i % (steps1/ (colors.length - 1))) / (steps1 / (colors.length - 1));
+        var weight = (i % (steps1 / (colors.length - 1))) / (steps1 / (colors.length - 1));
         var color = interpolateColor(startColor, endColor, weight);
         gradient.push(color);
     }
